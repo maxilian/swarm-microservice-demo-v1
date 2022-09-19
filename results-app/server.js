349063789bdb9bb2ebf34a9ec44e3cbd84ec3906
Lian Aga Aditya <shellovelyan@gmail.com>
2022-09-19 12:15:38 +0700

    Update server.js

--- results-app/server.js
+++ results-app/server.js
@@ -42,7 +42,7 @@ async.retry(
 
 function getVotes(client) {
   var allVotes = [];
-  client.query('SELECT id, vote, ts FROM votes', [], function(err, result) {
+  client.query('SELECT id, vote, ts FROM public.votes', [], function(err, result) {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
@@ -53,7 +53,7 @@ function getVotes(client) {
     }
   });
 
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+  client.query('SELECT vote, COUNT(id) AS count FROM public.votes GROUP BY vote', [], function(err, result) {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
